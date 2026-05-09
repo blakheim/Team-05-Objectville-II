7d7f6bb6bec6745f97793e64f840f006537cff96
Nadir Deniz Güzin <deniz@Nadir-MacBook-Pro.local>
2026-05-09 18:47:48 +0300

    Implemented abstract cell class

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,1 +1,23 @@
+package model;
 
+public abstract class Cell {
+
+    protected int row;
+    protected int col;
+
+    public Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public abstract char getSymbol();
+    public abstract boolean isPassable();
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}

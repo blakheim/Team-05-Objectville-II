f812c05b9fcbc4a04be790fa5f65f67a306679c6
Nadir Deniz Güzin <deniz@Nadir-MacBook-Pro.local>
2026-05-29 13:48:37 +0300

    Added Constants class

---
+++ src/model/Constants.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Constants {
+    public static final String ELECTRICITY = "electricity";
+    public static final String WATER = "water";
+    public static final String INTERNET = "internet";
+    public static final String SECURITY = "security";
+    public static final String HEALTH = "health";
+    public static final String EDUCATION = "education";
+    public static final String POPULATION = "population";
+    public static final String GOODS = "goods";
+    public static final String LIFESTYLE = "lifestyle";
+}
